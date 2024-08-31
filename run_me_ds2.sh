a816a6ead51fbd825727b549a838e9770d519207
Alex Nikolich <alexdragannikolich@gmail.com>
2024-08-31 18:55:33 +0000

    add ds2

---
+++ run_me_ds2.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES=0,3 PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:256 \
+accelerate launch \
+        --config_file configs/deepspeed.yaml \
+        train.py --config configs/asr_tts.yaml
