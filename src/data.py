9c0de58f30b48fae4961e14b70efff8c00898c61
Alex Nikolich <alexdragannikolich@gmail.com>
2024-08-31 18:08:32 +0000

    fix device

--- src/data.py
+++ src/data.py
@@ -6,6 +6,7 @@ class Vikhr4oDataset(Dataset):
         self.dataset = dataset
         self.tokenizer = tokenizer
         self.asr = asr
+        self.device = device
         self.n_original_tokens = len(tokenizer) - 1024
 
     def __len__(self):
@@ -15,7 +16,7 @@ class Vikhr4oDataset(Dataset):
         row = self.dataset[idx]
         text = row["text"]
         text_tokenized = self.tokenizer(text, return_tensors="pt")
-        text_input_tokens = text_tokenized["input_ids"].to(device)
+        text_input_tokens = text_tokenized["input_ids"].to(self.device)
 
         return {
             "text_input_tokens": text_input_tokens,
