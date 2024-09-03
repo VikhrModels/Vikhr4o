f7de7261adba6d0dec9fe3a5a7f8f01a0e949e4b
Alex Wortega <alexdragannikolich@gmail.com>
2024-09-04 01:20:55 +0200

    Update run_me_multi.sh

--- run_me_multi.sh
+++ run_me_multi.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0,3 PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:256 \
+CUDA_VISIBLE_DEVICES=0,3 NCCL_IB_GID_INDEX=3 NCCL_P2P_DISABLE=1 NCCL_P2P_LEVEL=NVL PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:256 \
 accelerate launch \
         --config_file configs/accelerate_multigpu.yaml \
         train.py --config configs/asr_tts.yaml
